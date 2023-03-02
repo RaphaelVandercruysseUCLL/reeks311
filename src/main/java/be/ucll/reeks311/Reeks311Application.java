2bf67914d1f8d9539ddeb64797ee50aa7e0df820
RaphaelVandercruysseUCLL <raphael.vandercruysse@student.ucll.be>
2023-03-02 11:37:31 +0100

    first commit

---
+++ src/main/java/be/ucll/reeks311/Reeks311Application.java
@@ -0,0 +1,13 @@
+package be.ucll.reeks311;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reeks311Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Reeks311Application.class, args);
+	}
+
+}
---
+++ src/test/java/be/ucll/reeks311/Reeks311ApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ucll.reeks311;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Reeks311ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
